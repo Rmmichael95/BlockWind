8c3aefcaef37c90404b326258af18bca37e48e84
Ryan M Sullivan <rmmichael95@gmail.com>
2026-02-17 22:07:31 -0800

    Started styling defaults

--- functions.php
+++ functions.php
@@ -1,4 +1,47 @@
 <?php
+/**
+ * This file adds functions to the BlockWind WordPress theme.
+ *
+ * @package BlockWind
+ * @author  Ryan M Sullivan
+ * @license GNU General Public License v2
+ * @link    https://github.com/Rmmichael95/BlockWind/
+ */
+
+
+if ( ! function_exists( 'blockwind_setup' ) ) {
+
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 *
+	 * @since 0.8.0
+	 *
+	 * @return void
+	 */
+	function blockwind_setup() {
+
+		// Make theme available for translation.
+		load_theme_textdomain( 'blockwind', get_template_directory() . '/languages' );
+
+		// Enqueue editor stylesheet.
+		add_editor_style( get_template_directory_uri() . '/style.css' );
+
+		// Remove core block patterns.
+		// remove_theme_support( 'core-block-patterns' );
+	}
+}
+add_action( 'after_setup_theme', 'blockwind_setup' );
+
+// Enqueue stylesheet.
+add_action( 'wp_enqueue_scripts', 'blockwind_enqueue_stylesheet' );
+function blockwind_enqueue_stylesheet() {
+	wp_enqueue_style( 'blockwind', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+}
+
 add_action(
 	'wp_enqueue_scripts',
 	function () {
